68afbe6b48c1a3b5e23d000761c171f0adde064a
HigorDenomar <higordenomar@hotmail.com>
2020-02-15 07:33:31 -0300

    Add Discord Icon

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const { app, BrowserWindow, Tray } = require('electron');
+const { app, BrowserWindow} = require('electron');
 
 let win;
 
 function createWindow() {
 
-  const appIcon = new Tray('./src/assets/discord-icon.png')
-
   win = new BrowserWindow({
     minWidth: 960,
     minHeight: 530,
+    icon: './src/assets/discord-icon.png',
     frame: false,
     
   });
